8aa3320cf89c4ed7a2b1257f3dd96aaf5111a7b6
Daniela Almeida <danielaspbrasil@gmail.com>
2023-09-14 21:47:13 -0300

    Adicionado dependencia Lombok

--- src/main/java/br/com/fintech/model/Gasto.java
+++ src/main/java/br/com/fintech/model/Gasto.java
@@ -3,9 +3,15 @@ package br.com.fintech.model;
 import br.com.fintech.model.dto.GastoRegisterData;
 import br.com.fintech.model.dto.GastoUpdateData;
 import jakarta.persistence.*;
+import lombok.*;
+
 
 @Entity
 @Table(name = "gasto" )
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Gasto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,44 +21,11 @@ public class Gasto {
     private String descricao;
 
 
-    //Constructs
-    public Gasto() {}
-
-    public Gasto(Long id, Long valor, String descricao) {
-        this.id = id;
-        this.valor = valor;
-        this.descricao = descricao;
-    }
-
+    //Register
     public Gasto(GastoRegisterData data) {
         this.valor = data.valor();
         this.descricao = data.descricao();
     }
-
-
-
-    //GetsSets
-    public Long getId() {
-        return id;
-    }
-
-    public Long getValor() {
-        return valor;
-    }
-
-    public void setValor(Long valor) {
-        this.valor = valor;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-
 
     //Update
     public void updateGasto(GastoUpdateData data) {
